c280e6c25ed7ba6e5b0e58ff06c38a47e7275647
thulijay <thulijay79@gmail.com>
2021-05-20 11:44:33 +0200

    added spec

---
+++ src/bicycles/BicycleFromSpec.java
@@ -0,0 +1,24 @@
+package bicycles;
+
+public class BicycleFromSpec extends BicycleBase {
+        public BicycleSpecification bicycleSpecification;
+
+        public BicycleFromSpec(BicycleSpecification bicycleSpecification){
+            this.bicycleSpecification = bicycleSpecification;
+        }
+
+        @Override
+        public void accelerate() {
+            this.changeSpeed(this.bicycleSpecification.getAccelerationSpeed());
+        }
+
+        @Override
+        public void brake() {
+            this.changeSpeed(this.bicycleSpecification.getBrakeSpeed());
+        }
+
+    @Override
+    public BicycleType getBicycleType() {
+        return null;
+    }
+}
---
+++ src/bicycles/BicycleSpecification.java
@@ -0,0 +1,31 @@
+package bicycles;
+
+
+public class BicycleSpecification {
+    private  BicycleType bicycleType;
+    private  int accelerationSpeed;
+    private  int brakeSpeed;
+
+//    BicycleSpecification roadBikeSpec = new BicycleSpecification(11, 4);
+
+
+    public BicycleSpecification(int accelerationSpeed, int brakeSpeed, BicycleType bicycleType){
+        this.accelerationSpeed = accelerationSpeed;
+        this.brakeSpeed = brakeSpeed;
+        this.bicycleType = bicycleType;
+
+//        System.out.println(roadBikeSpec.getBrakeSpeed());
+    }
+
+    public int getAccelerationSpeed(){
+        return accelerationSpeed;
+    }
+
+    public int getBrakeSpeed(){
+        return brakeSpeed;
+    }
+
+    public BicycleType getBicycleType(){
+        return bicycleType;
+    }
+}
